56bb4a7eb620a25ea49fd4eb37b5ef5d4687a84c
5k-omar <92945935+5k-omar@users.noreply.github.com>
2023-06-12 18:04:31 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+[
+  {
+    imageUrl: 'https://trackercdn.com/legacycdn/fortnite/289A12395_large.png',
+    manifestId: 12395,
+    name: 'Tigeress',
+    rarity: 'Rare',
+    storeCategory: 'BRSpecialFeatured',
+    vBucks: 1200
+  },
+  {
+    imageUrl: 'https://trackercdn.com/legacycdn/fortnite/B8EF12830_small.png',
+    manifestId: 12830,
+    name: 'Tiniest Violin',
+    rarity: 'uncommon',
+    storeCategory: 'BRDailyStorefront',
+    vBucks: 200
+  }
+]
